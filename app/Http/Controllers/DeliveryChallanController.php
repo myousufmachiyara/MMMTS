eb97c027f2fe4c1ffdad661e4495939f4a4e102c
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-04 16:37:12 +0500

    Update DeliveryChallanController.php

--- app/Http/Controllers/DeliveryChallanController.php
+++ app/Http/Controllers/DeliveryChallanController.php
@@ -204,9 +204,9 @@ class DeliveryChallanController extends Controller
         $pdf->SetXY(10, 10);
         $pdf->Cell(0, 7, 'M M LOGISTICS', 0, 1, 'L');
         $pdf->SetFont('helvetica', '', 9);
-        $pdf->SetXY(40, 17);
+        $pdf->SetXY(10, 17);
         $pdf->Cell(0, 5, 'Room No 301, 303, 305, 307, 3rd Floor, Custom Trade Tower,', 0, 1, 'L');
-        $pdf->SetXY(40, 22);
+        $pdf->SetXY(10, 22);
         $pdf->Cell(0, 5, 'KPT Stadium, Kharadar, Karachi', 0, 1, 'L');
 
         $pdf->SetFont('helvetica', 'B', 14);
