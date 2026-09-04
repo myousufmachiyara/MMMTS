4505a6d35f9655a877ca17d5d18000b174c52554
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-04 17:51:53 +0500

    Update BillController.php

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -264,11 +264,6 @@ class BillController extends Controller
         $pdf->AddPage();
         $pdf->setCellPadding(1.5);
 
-        $logoPath = public_path('assets/img/logo.png');
-        if (file_exists($logoPath)) {
-            $pdf->Image($logoPath, 12, 8, 40);
-        }
-
         $pdf->SetFont('helvetica', 'B', 14);
         $pdf->SetXY(120, 12);
         $pdf->Cell(80, 8, 'BILL', 0, 1, 'R');
